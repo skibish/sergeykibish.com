82c8655243a8893a6776c798cb2bc0024d79cfad
Sergey Kibish <sergey.kibish@gmail.com>
2020-05-31 14:48:15 +0300

    Add mising classes, change what files to ignore during build

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [ './hugo_stats.json' ],
+  content: ['./hugo_stats.json'],
   defaultExtractor: (content) => {
-      let els = JSON.parse(content).htmlElements;
-      return els.tags.concat(els.classes, els.ids);
+    let els = JSON.parse(content).htmlElements;
+    els.classes.push('testimonials__testimonial--expanded', 'expanded');
+    return els.tags.concat(els.classes, els.ids);
   }
 });
 
