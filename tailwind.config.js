b0c92c16ccf117203f753b69054a553b9c2bf8b8
Sergey Kibish <5479211+skibish@users.noreply.github.com>
2022-05-08 17:17:16 +0300

    add dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         main: "#2171a4",
         accent: "#c7ba9d",
         txt: "#f8f5f3",
+        darkMain: "#0e131f",
+        darkAccent: "#ab966d",
+        darkTxt: "#909cc2"
       }
     },
   },
